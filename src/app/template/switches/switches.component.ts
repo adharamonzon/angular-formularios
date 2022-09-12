3851586d09d7013315f513dc1a7266f496274bc0
adhara monzon <adhara.monzonvarillas@everis.nttdata.com>
2022-09-12 15:05:31 +0200

    add radio, check and switch form

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SwitchesComponent {
+ user = {
+  gender: 'f',
+  notifications: true,
+  terms: false
+ }
 }
