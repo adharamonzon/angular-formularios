c6d3c640479a4a501d6eb787d0a3bc78c4a84dcb
adhara monzon <adhara.monzonvarillas@everis.nttdata.com>
2022-09-13 14:34:44 +0200

    add functionality to radio, select and check

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  constructor( private fb : FormBuilder) { }
+    switchForm : FormGroup = this.fb.group({
+      gender: ['F', Validators.required],
+      notifications: [true, Validators.required],
+      terms: [false, Validators.requiredTrue]
+    })
 
+    user = {
+      gender: 'M',
+      notifications: true
+    }
   ngOnInit(): void {
+    this.switchForm.reset({
+      ...this.user,
+      terms: false
+    })
+    //para que se actualice el formulario segun se cambia, no dandole al btn
+    /* this.switchForm.valueChanges.subscribe(form => {
+      console.log(form);
+    }) */
   }
 
+  save() {
+    const formValue = {...this.switchForm.value};
+    this.user = formValue
+  }
 }
